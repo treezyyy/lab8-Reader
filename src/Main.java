c06d94414632dad11e4c045792923d870c0e211e
treezyyy <122749977+treezyyy@users.noreply.github.com>
2023-03-09 19:40:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        reader[] array = new reader[5];
+        for(int i = 0; i < 5; i++){
+           System.out.println("Введите ФИО");
+           String initials = in.next();
+           System.out.println("Введите факулььтет");
+           String facult = in.next();
+           System.out.println("Введите дату рождения");
+           String bithdate = in.next();
+           System.out.println("Введите номер телефона");
+           String phone = in.next();
+           array[i] = new reader(initials, facult, bithdate, phone);
+        }
+        String[] collections = new String[] {"Пушкин", "Высоцкий", "Есенин", "Малышев"};
+        System.out.println(Arrays.toString(array));
+        System.out.println(array[0].TakeBook(4));
+        System.out.println(array[0].TakeBook(collections));
+        System.out.println(array[0].ReturnBook(4));
+        System.out.println(array[0].ReturnBook(collections));
+    }
+}
---
+++ src/reader.java
@@ -0,0 +1,29 @@
+public class reader {
+    String initials;
+    int TicketNumber;
+    String facult;
+    String birthdate;
+    String phone;
+    int CounterTicketNumber = 1;
+    reader (String initials, String birthdate, String phone, String facult){
+        this.initials = initials; // for ezempl
+        this.facult = facult;
+        this.birthdate = birthdate;
+        this.phone = phone;
+        TicketNumber = CounterTicketNumber++;
+    }
+    public String TakeBook(int QuantityBook){
+        return this.initials + " взял " + QuantityBook + " книг";
+    }
+    public String TakeBook(String[] NamedBook){
+        String Collection = String.join(",", NamedBook);
+        return this.initials + " взял " + Collection;
+    }
+    public String ReturnBook(int QuantityBook){
+        return this.initials + " Вернул " + QuantityBook + " книг";
+    }
+    public String ReturnBook(String[] NamedBook){
+        String Collection = String.join(",", NamedBook);
+        return this.initials + " Вернул " + Collection + " книг ";
+    }
+}
